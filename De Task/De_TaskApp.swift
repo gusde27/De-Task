9fdea90be61661868e11557073dbea8f3c3df04e
gusde27 <gusdechang24@gmail.com>
2022-07-05 14:23:43 +0800

    Initial Commit

---
+++ De Task/De_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  De_TaskApp.swift
+//  De Task
+//
+//  Created by I Gede Bagus Wirawan on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct De_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
